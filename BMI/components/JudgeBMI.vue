3c1d4ea96b2a96d71527e59843c3a8bc84b9be84
mz-9905 <92282434+mz-9905@users.noreply.github.com>
2023-03-30 19:38:16 +0900

    Add files via upload

---
+++ BMI/components/JudgeBMI.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="p-3">
+        <div v-if="bmi<=0.0" class="p-3 mb-2 bg-light bg-gradient text-dark">-</div>
+        <div v-else-if="bmi<18.5" class="p-3 mb-2 bg-success bg-gradient text-white">やせ</div>
+        <div v-else-if="bmi<25.0" class="p-3 mb-2 bg-primary bg-gradient text-white">標準</div>
+        <div v-else-if="bmi<30.0" class="p-3 mb-2 bg-warning bg-gradient text-white">軽度肥満</div>
+        <div v-else class="p-3 mb-2 bg-danger bg-gradient text-white">重度肥満</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['bmi'],
+}
+</script>
---
+++ BMI/components/ShowResults.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="p-3">
+        <h1>{{appName}}</h1>
+        <div class="mb-3">{{message}}</div>
+        <div class="mb-3">
+            計算式: {{kg}} / ({{cm}} * 0.01)^2<br/>
+            BMI値: {{bmi}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['appName','message','kg','cm','bmi'],
+}
+</script>
