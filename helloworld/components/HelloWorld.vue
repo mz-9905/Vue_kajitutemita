b6a9b3d8177f1f7169315091e9013aeb2ffd1af5
mz-9905 <92282434+mz-9905@users.noreply.github.com>
2023-03-30 19:41:46 +0900

    Add files via upload

---
+++ helloworld/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="m-3 p-3">
+        <h1>{{appName}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HelloWorld",
+    data(){
+        return {
+            appName:"Hello Vue.js!"
+        }
+    }
+}
+</script>
