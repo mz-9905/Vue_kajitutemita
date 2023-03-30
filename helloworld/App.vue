dd89a1471c7ba51cf088ae220abc1b0b23b95a7d
mz-9905 <92282434+mz-9905@users.noreply.github.com>
2023-03-30 19:42:13 +0900

    Add files via upload

---
+++ helloworld/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app" class=".container">
+        <div class="example">
+            <h1>test</h1>
+        </div>
+        <HelloWorld/>
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default{
+    name:"App",
+    components:{ HelloWorld }
+}
+</script>
+
+<style scoped>
+.example {
+    color: red;
+    text-align: center;
+}
+</style>
